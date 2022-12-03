de2ea5a6d39a3d05599a15a31d4b2c8da64b620f
Dmitry <dimarik6977@yandex.ru>
2022-12-04 01:44:00 +0300

    Kata_Test

---
+++ src/main/java/App.java
@@ -0,0 +1,32 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+
+        boolean result = bean == bean2;
+        System.out.println(result);
+
+        boolean result2 = cat == cat2;
+        System.out.println(result2);
+
+    }
+}
